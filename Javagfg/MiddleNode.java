bc486275bf4ec67e1a9b1ea840b5beb076420174
Ritik Khokar <khokharritik1999@gmail.com>
2024-05-20 22:28:49 +0530

    MiddleNode

---
+++ Javagfg/MiddleNode.java
@@ -0,0 +1,22 @@
+
+class Node {
+    int data;
+     Node next;
+     Node(int d)  { data = d;  next = null; }
+ }
+
+
+class MiddleNode
+{
+    int getMiddle(Node head)
+    {
+         // Your code here.
+         Node fast = head;
+         Node slow = head;
+         while(fast != null && fast.next != null){
+             slow = slow.next;
+             fast = fast.next.next;
+         }
+         return slow.data;
+    }
+}
